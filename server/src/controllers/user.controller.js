2a88a7e023cbc27de8f761d633dcb0de85c8b174
Beprodeep Das <codingbtech@gmail.com>
2025-04-14 11:06:17 +0530

    cookie2

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -93,7 +93,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       maxAge: 30 * 24 * 60 * 60 * 1000,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
     });
     res.status(200).json({
       _id: user._id,
