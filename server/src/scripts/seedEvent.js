51c9c6827c54ce0920491baf91da6d347f6051bd
Beprodeep Das <codingbtech@gmail.com>
2025-04-21 12:49:33 +0530

    seedEvent

--- server/src/scripts/seedEvent.js
+++ server/src/scripts/seedEvent.js
@@ -7,57 +7,56 @@ dotenv.config();
 
 const events = [
   {
-    name: "Quiz",
-    type: "individual",
-    description:
-      "A competitive coding challenge to test problem-solving skills.",
-    date: new Date("2025-04-25T21:00:00Z"),
+    name: "Electroquizon",
+    type: "team",
+    description: "A Technical Quiz Competition on Electronics",
+    date: new Date("2025-04-25T15:30:00Z"), // 9:00 PM IST
     maxTeamMembers: 2,
     posterUrl:
-      "https://res.cloudinary.com/dltygrzd4/image/upload/v1744521825/Art_Poster_-_Sicario_-_Tallenge_Hollywood_Collection_large_jocqew.webp",
+      "https://res.cloudinary.com/dltygrzd4/image/upload/v1745217050/IMG-20250420-WA0012_fbazx3.jpg",
   },
   {
     name: "TinkerForge",
-    type: "individual",
-    description: "Fix broken code as fast as you can.",
-    date: new Date("2025-04-26T10:00:00Z"),
+    type: "team",
+    description: "Crafting Connections That Last: Show your skills on soldering",
+    date: new Date("2025-04-26T04:30:00Z"), // 10:00 AM IST
     maxTeamMembers: 4,
     posterUrl:
-      "https://res.cloudinary.com/dltygrzd4/image/upload/v1744521825/Art_Poster_-_Sicario_-_Tallenge_Hollywood_Collection_large_jocqew.webp",
+      "https://res.cloudinary.com/dltygrzd4/image/upload/v1745217050/IMG-20250420-WA0009_hwh2vc.jpg",
   },
   {
     name: "Capture the Fault",
-    type: "team",
-    description: "Build something cool in a short amount of time.",
-    date: new Date("2025-04-26T14:00:00Z"),
-    maxTeamMembers: 4,
+    type: "individual",
+    description: "Diagnosing Circuit Woes",
+    date: new Date("2025-04-26T08:30:00Z"), // 2:00 PM IST
+    maxTeamMembers: 1,
     posterUrl:
       "https://res.cloudinary.com/dltygrzd4/image/upload/v1745217051/IMG-20250420-WA0011_tlbbuu.jpg",
   },
   {
     name: "TechTussle",
     type: "individual",
-    description: "Design an intuitive and aesthetic interface for a mock app.",
-    date: new Date("2025-04-26T15:15:00Z"),
+    description: "Debate Competition on the topics of Electronics",
+    date: new Date("2025-04-26T09:45:00Z"), // 3:15 PM IST
     maxTeamMembers: 1,
     posterUrl:
-      "https://res.cloudinary.com/dltygrzd4/image/upload/v1744521825/Art_Poster_-_Sicario_-_Tallenge_Hollywood_Collection_large_jocqew.webp",
+      "https://res.cloudinary.com/dltygrzd4/image/upload/v1745217050/IMG-20250420-WA0010_wbzef6.jpg",
   },
   {
-    name: "Ohm's kitchen",
-    type: "team",
-    description: "Put your tech knowledge to the test in a thrilling quiz.",
-    date: new Date("2025-04-26T16:30:00Z"),
-    maxTeamMembers: 2,
+    name: "Ohm's Kitchen",
+    type: "individual",
+    description: "Combine the components to cook some electronics-dish",
+    date: new Date("2025-04-26T11:00:00Z"), // 4:30 PM IST
+    maxTeamMembers: 1,
     posterUrl:
-      "https://res.cloudinary.com/dltygrzd4/image/upload/v1744521825/Art_Poster_-_Sicario_-_Tallenge_Hollywood_Collection_large_jocqew.webp",
+      "https://res.cloudinary.com/dltygrzd4/image/upload/v1745219455/WhatsApp_Image_2025-04-21_at_12.31.02_ecc56fa8_uqki2r.jpg",
   },
   {
     name: "9/11",
-    type: "team",
-    description: "Race your bots through a challenging course.",
-    date: new Date("2025-04-26T18:45:00Z"),
-    maxTeamMembers: 3,
+    type: "individual",
+    description: "Catastrophic Devastation",
+    date: new Date("2025-04-26T13:15:00Z"), // 6:45 PM IST
+    maxTeamMembers: 1,
     posterUrl:
       "https://res.cloudinary.com/dltygrzd4/image/upload/v1745217050/IMG-20250420-WA0008_virpgn.jpg",
   },
